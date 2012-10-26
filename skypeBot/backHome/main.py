15060eceb49b88610287fcdff61f101691e79701
cassy <takahirokashiwagi.cassy@gmail.com>
2012-10-26 23:39:34 +0900

    do not show bot message on Saturday and Sunday

--- skypeBot/backHome/main.py
+++ skypeBot/backHome/main.py
@@ -78,35 +78,36 @@ skype.OnMessageStatus = handler
 
 while(True):
     now = datetime.datetime.now()
-    if (skype.Chats[chat_id].Topic == u'帰らせマスターから貴方へ伝えたいこと'):
+    if now.weekday() != 5 and now.weekday() != 6:
+        if (skype.Chats[chat_id].Topic == u'帰らせマスターから貴方へ伝えたいこと'):
 
-        if (now.second == 0):
-            if (now.hour == 9):
-                if (now.minute == 30):
-                    skype.Chats[chat_id].SendMessage(now.strftime('おはよーさん！%m月%d日が始まんで！今日も１日きばっていこやっ(flex)'))
-                    time.sleep(10)
-            elif (now.hour == 21):
-                if (now.minute > 50):
-                    skype.Chats[chat_id].SendMessage(now.strftime('%M分'))
-                    time.sleep(10)
-                elif (now.minute >= 30 & now.minute % 10 == 0):
-                    leftTime = 60 - now.minute
-                    message = '22時まであと' + str(leftTime) + '分やで！？'
-                    skype.Chats[chat_id].SendMessage(message)
-                    time.sleep(10)
-            elif (now.hour == 22):
-                if (now.minute == 0):
-                    skype.Chats[chat_id].SendMessage(now.strftime('%H時になったで！おつかれさん！そろそろ帰ってやー(bow)'))
-                    time.sleep(10)
-                elif (now.minute % 10 == 0):
-                    skype.Chats[chat_id].SendMessage(now.strftime('%H時を%M分過ぎてもうたで！はよ帰ってや！(envy)'))
-                    time.sleep(10)
-            elif (now.hour == 23):
-                if (now.minute == 0):
-                    skype.Chats[chat_id].SendMessage(now.strftime('%H時やで！こんな時間までおるん！？もうお前なんて知らんわ(punch)'))
-                    time.sleep(10)
-    else:
-        chat_id = getChatId()
+            if (now.second == 0):
+                if (now.hour == 9):
+                    if (now.minute == 30):
+                        skype.Chats[chat_id].SendMessage(now.strftime('おはよーさん！%m月%d日が始まんで！今日も１日きばっていこやっ(flex)'))
+                        time.sleep(10)
+                elif (now.hour == 21):
+                    if (now.minute > 50):
+                        skype.Chats[chat_id].SendMessage(now.strftime('%M分'))
+                        time.sleep(10)
+                    elif (now.minute >= 30 & now.minute % 10 == 0):
+                        leftTime = 60 - now.minute
+                        message = '22時まであと' + str(leftTime) + '分やで！？'
+                        skype.Chats[chat_id].SendMessage(message)
+                        time.sleep(10)
+                elif (now.hour == 22):
+                    if (now.minute == 0):
+                        skype.Chats[chat_id].SendMessage(now.strftime('%H時になったで！おつかれさん！そろそろ帰ってやー(bow)'))
+                        time.sleep(10)
+                    elif (now.minute % 10 == 0):
+                        skype.Chats[chat_id].SendMessage(now.strftime('%H時を%M分過ぎてもうたで！はよ帰ってや！(envy)'))
+                        time.sleep(10)
+                elif (now.hour == 23):
+                    if (now.minute == 0):
+                        skype.Chats[chat_id].SendMessage(now.strftime('%H時やで！こんな時間までおるん！？もうお前なんて知らんわ(punch)'))
+                        time.sleep(10)
+        else:
+            chat_id = getChatId()
 
 """
 #skype_name = msg.FromHandle
